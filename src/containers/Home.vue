1717fdca14076de6df6102c3a6ca064824cec913
Geovana Silva <sts.geovana@gmail.com>
2020-06-09 00:22:55 -0300

    feat(init): project init
    * create index.html and app.js
    * create home component
    * create svg logo

---
+++ src/containers/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="app">
+    <img src="@assets/img/hc.svg" />
+    <h1>UX Designer | Product Designer</h1>
+    <h2>
+      Sempre curioso e com gosto pela experimentação, meu objetivo é projetar
+      produtos que causem impacto nas pessoas, afinal projetar experiencias é e
+      sempre será sobre pessoas.
+    </h2>
+    <a href="#">Ver minha experiencia completa ></a>
+  </div>
+</template>
+<style>
+body {
+  color: #464646;
+  font-family: "Roboto", sans-serif;
+  width: 100%;
+  margin: 0;
+}
+
+.app {
+  text-align: center;
+  margin-top: 120px;
+}
+
+.app > img {
+  width: 330px;
+}
+
+h1 {
+  font-size: 16px;
+  font-weight: 300;
+}
+
+h2 {
+  font-size: 14px;
+  line-height: 16px;
+  font-weight: 300;
+  width: 650px;
+  margin: auto;
+  margin-top: 130px;
+  margin-bottom: 30px;
+}
+
+a {
+  color: #0029ff;
+  font-size: 14px;
+}
+</style>
