a12aa218779a547610dde0ab70af841cb182f5bb
Geovana Silva <sts.geovana@gmail.com>
2020-06-09 00:19:01 -0300

    feat(config): babel, editor, gitignore and webpack

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+var path = require("path");
+var webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "/dist/",
+    filename: "build.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["vue-style-loader", "css-loader"]
+      },
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+        options: {
+          loaders: {}
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]?[hash]"
+        }
+      }
+    ]
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, "./src"), "node_modules"],
+    alias: {
+      vue$: "vue/dist/vue.esm.js",
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@containers": path.resolve(__dirname, "./src/containers")
+    },
+    extensions: ["*", ".js", ".vue", ".json"]
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true,
+    overlay: true
+  },
+  performance: {
+    hints: false
+  },
+  devtool: "#eval-source-map"
+};
+
+if (process.env.NODE_ENV === "production") {
+  module.exports.devtool = "#source-map";
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ]);
+}
